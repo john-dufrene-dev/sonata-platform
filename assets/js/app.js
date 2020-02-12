779382ec2e0ac449c5e96b92f138da3896291a0d
John Dufrene <john.dufrene.pro@gmail.com>
2020-02-12 21:54:32 +0100

    Add webpack config

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.scss';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+import $ from 'jquery';
+
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
