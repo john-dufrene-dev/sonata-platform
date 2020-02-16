8449fcd94d09a17c0c1dd999a8f722da447f4720
John Dufrene <john.dufrene.pro@gmail.com>
2020-02-16 23:46:42 +0100

    Add package Google-authenticator for sonata

--- src/Bundle/Sonata/UserBundle/Admin/Model/GroupAdmin.php
+++ src/Bundle/Sonata/UserBundle/Admin/Model/GroupAdmin.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace App\Bundle\Sonata\UserBundle\Admin\Entity;
+namespace App\Bundle\Sonata\UserBundle\Admin\Model;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
