9eb8a4163466501f63f5a58c2f90405d4d02526b
John Dufrene <john.dufrene.pro@gmail.com>
2020-04-08 21:06:54 +0200

    Custom Access to UserAdmin

--- src/Admin/UserAdmin/UserAdmin.php
+++ src/Admin/UserAdmin/UserAdmin.php
@@ -13,9 +13,160 @@ declare(strict_types=1);
 
 namespace App\Admin\UserAdmin;
 
+use Sonata\Form\Type\DatePickerType;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\AdminBundle\Form\Type\ModelType;
+use App\Form\Admin\Type\CustomSecurityRolesType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
+use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
 use Sonata\UserBundle\Admin\Model\UserAdmin as BaseUserAdmin;
 
 final class UserAdmin extends BaseUserAdmin
 {
     public $supportsPreviewMode = true;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureShowFields(ShowMapper $showMapper): void
+    {
+        $showMapper
+            ->with('General')
+                ->add('username')
+                ->add('email')
+            ->end()
+            ->with('Groups')
+                ->add('groups')
+            ->end()
+            ->with('Profile')
+                ->add('dateOfBirth')
+                ->add('firstname')
+                ->add('lastname')
+                ->add('website')
+                ->add('biography')
+                ->add('gender')
+                ->add('locale')
+                ->add('timezone')
+                ->add('phone')
+            ->end()
+            ->with('Social')
+                // ->add('facebookUid')
+                ->add('facebookName')
+                // ->add('twitterUid')
+                ->add('twitterName')
+                // ->add('gplusUid')
+                // ->add('gplusName')
+            ->end()
+        ;
+        if ($this->isGranted('ROLE_SUPER_ADMIN')) {
+            $showMapper
+                ->with('Security')
+                    ->add('token')
+                    ->add('twoStepVerificationCode')
+                ->end()
+            ;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper): void
+    {
+        // define group zoning
+        $formMapper
+            ->tab('User')
+                ->with('Profile', ['class' => 'col-md-6'])->end()
+                ->with('General', ['class' => 'col-md-6'])->end()
+                ->with('Social', ['class' => 'col-md-6'])->end()
+            ->end()
+        ;
+
+        if ($this->isGranted('ROLE_SUPER_ADMIN')) {
+            $formMapper
+                ->tab('Security')
+                    ->with('Status', ['class' => 'col-md-4'])->end()
+                    ->with('Groups', ['class' => 'col-md-4'])->end()
+                    ->with('Keys', ['class' => 'col-md-4'])->end()
+                    ->with('Roles', ['class' => 'col-md-12'])->end()
+                ->end()
+            ;
+        }
+
+        $now = new \DateTime();
+
+        $genderOptions = [
+            'choices' => \call_user_func([$this->getUserManager()->getClass(), 'getGenderList']),
+            'required' => true,
+            'translation_domain' => $this->getTranslationDomain(),
+        ];
+
+        $formMapper
+            ->tab('User')
+                ->with('General')
+                    ->add('username')
+                    ->add('email')
+                    ->add('plainPassword', TextType::class, [
+                        'required' => (!$this->getSubject() || null === $this->getSubject()->getId()),
+                    ])
+                ->end()
+                ->with('Profile')
+                    ->add('dateOfBirth', DatePickerType::class, [
+                        'years' => range(1900, $now->format('Y')),
+                        'dp_min_date' => '1-1-1900',
+                        'dp_max_date' => $now->format('c'),
+                        'required' => false,
+                    ])
+                    ->add('firstname', null, ['required' => false])
+                    ->add('lastname', null, ['required' => false])
+                    ->add('website', UrlType::class, ['required' => false])
+                    ->add('biography', TextType::class, ['required' => false])
+                    ->add('gender', ChoiceType::class, $genderOptions)
+                    ->add('locale', LocaleType::class, ['required' => false])
+                    ->add('timezone', TimezoneType::class, ['required' => false])
+                    ->add('phone', null, ['required' => false])
+                ->end()
+                ->with('Social')
+                    // ->add('facebookUid', null, ['required' => false])
+                    ->add('facebookName', null, ['required' => false])
+                    // ->add('twitterUid', null, ['required' => false])
+                    ->add('twitterName', null, ['required' => false])
+                    // ->add('gplusUid', null, ['required' => false])
+                    // ->add('gplusName', null, ['required' => false])
+                ->end()
+            ->end()
+            ;
+        if ($this->isGranted('ROLE_SUPER_ADMIN')) {
+            $formMapper
+                ->tab('Security')
+                    ->with('Status')
+                        ->add('enabled', null, ['required' => false])
+                    ->end()
+                    ->with('Groups')
+                        ->add('groups', ModelType::class, [
+                            'required' => false,
+                            'expanded' => true,
+                            'multiple' => true,
+                        ])
+                    ->end()
+                    ->with('Roles')
+                        ->add('realRoles', CustomSecurityRolesType::class, [
+                            'label' => 'form.label_roles',
+                            'expanded' => true,
+                            'multiple' => true,
+                            'required' => false,
+                        ])
+                    ->end()
+                    ->with('Keys')
+                        ->add('token', null, ['required' => false])
+                        ->add('twoStepVerificationCode', null, ['required' => false])
+                    ->end()
+                ->end()
+            ;
+        }
+    }
 }
--- src/Controller/Admin/UserAdmin/UserAdminController.php
+++ src/Controller/Admin/UserAdmin/UserAdminController.php
@@ -5,8 +5,37 @@ declare(strict_types=1);
 namespace App\Controller\Admin\UserAdmin;
 
 use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 final class UserAdminController extends CRUDController
 {
-    //
+    /**
+     * setContainer
+     *
+     * @param  mixed $container
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+
+        $this->configure();
+
+        if ($this->admin->isCurrentRoute('edit')) {
+
+            $request = $this->getRequest();
+            $id = $request->get($this->admin->getIdParameter());
+
+            if (
+                !$this->isGranted('ROLE_SUPER_ADMIN')
+                && !$this->isGranted('ROLE_SONATA_USER_ADMIN_USER_ALL')
+            ) {
+                $currentUserEdit = (!$this->getUser() || $id == $this->getUser()->getId()) ? true : false;
+
+                if (!$currentUserEdit) {
+                    throw $this->createNotFoundException(sprintf('You can\'t edit this id : %s', $id));
+                }
+            }
+        }
+    }
 }
